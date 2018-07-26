3d2454d31064e453517a748deff6ceecbd7e2efa
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:41:43 +0200

    boucles

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>exo2</title>
+</head>
+<body>
+  <p>
+  <?php
+  $count= 0;
+  $number = 50;
+while ($count <= 20) {
+echo $count * $number;
+$count++;
+}
+  ?>
+</p>
+</body>
+</html>
